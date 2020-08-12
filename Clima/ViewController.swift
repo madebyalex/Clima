14f3a08db639dbf8413c3e672b1faf8c5e6638bc
Alexander <hello@alexander.works>
2020-08-13 00:22:09 +0300

    Adapted design for landscape view & Minor UI changes

--- Clima/ViewController.swift
+++ Clima/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text)
+    }
+    
 }
 
